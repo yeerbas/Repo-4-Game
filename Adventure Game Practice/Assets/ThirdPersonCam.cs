ce889422a345353adaba7680611ef5dcc40fbc83
yeerbas <jagoj420@gmail.com>
2020-08-12 11:36:30 +0100

    Game Files
    Part 1

---
+++ Adventure Game Practice/Assets/ThirdPersonCam.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCam : MonoBehaviour
+{
+
+	public bool lockCursor;
+	public float mouseSensitivity = 10;
+	public Transform target;
+	public float dstFromTarget = 2;
+	public Vector2 pitchMinMax = new Vector2(-40, 85);
+
+	public float rotationSmoothTime = .10f;
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRotation;
+
+	float yaw;
+	float pitch;
+
+	void Start()
+	{
+		if (lockCursor)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+
+	void LateUpdate()
+	{
+		yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+		pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+		pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+		currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+		transform.eulerAngles = currentRotation;
+
+		transform.position = target.position - transform.forward * dstFromTarget;
+
+	}
+
+}
